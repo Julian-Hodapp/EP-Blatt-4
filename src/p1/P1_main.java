aa56c345ed2e77a62ab849f79051de693673c0c0
Julian Hodapp <julian@MacBook-Pro-von-Julian.local>
2025-11-13 12:31:07 +0100

    Fix Aufgabe p1 (breaks im Switch entfernen und Reihenfolge anpassen

--- src/p1/P1_main.java
+++ src/p1/P1_main.java
@@ -2,7 +2,7 @@ package p1;
 
 public class P1_main {
 	public static void main(String[] args) {
-		int stockwert = 4;
+		int stockwert = 2;
 		
 		switch(stockwert) {
 		case 0:
@@ -25,22 +25,13 @@ public class P1_main {
 		}
 		
 		switch(stockwert) {
-		case 0:
-			System.out.println("Angebote");
-			break;
-		case 1:
-			System.out.println("Damenbekleidung");
-			System.out.println("Angebote");
-			break;
-		case 2:
-			System.out.println("Herrenbekleidung");
-			System.out.println("Damenbekleidung");
-			System.out.println("Angebote");
-			break;
 		case 3:
 			System.out.println("Kinderbekleidung");
+		case 2:
 			System.out.println("Herrenbekleidung");
+		case 1:
 			System.out.println("Damenbekleidung");
+		case 0:
 			System.out.println("Angebote");
 			break;
 		case -1:
